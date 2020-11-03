f3ef45247c16a73b009ed9276c2592209c29c25b
yutoVR <yuto.fushimi@icloud.com>
2020-11-03 17:50:34 +0900

    関連付けの非同期実行

--- APKInstaller/Association.cs
+++ APKInstaller/Association.cs
@@ -68,37 +68,41 @@ namespace APKInstaller
 
         public static void Associate() => Run($"Associate \"{Assembly.GetEntryAssembly().Location}\"");
         public static void Dissociate() => Run("Dissociate");
+        static async void Run(string argument) => await RunAsync(argument);
 
-        static void Run(string argument)
+        static async Task RunAsync(string argument)
         {
-            var isAssociate = argument.StartsWith("Associate");
+            await Task.Run(() =>
+            {
+                var isAssociate = argument.StartsWith("Associate");
 
-            var startInfo = new ProcessStartInfo
-            {
-                FileName = path,
-                Arguments = argument,
-                UseShellExecute = true,
-                CreateNoWindow = true,
-                WindowStyle = ProcessWindowStyle.Hidden,
-                Verb = "runas",
-            };
+                var startInfo = new ProcessStartInfo
+                {
+                    FileName = path,
+                    Arguments = argument,
+                    UseShellExecute = true,
+                    CreateNoWindow = true,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    Verb = "runas",
+                };
 
-            try
-            {
-                var process = Process.Start(startInfo);
-                process.WaitForExit();
-            }
-            catch (Win32Exception)
-            {
-                var failureMessage = isAssociate ? "APK を本アプリに関連付けられませんでした。" : "関連付けの解除に失敗しました。";
-                Message.Add(failureMessage);
+                try
+                {
+                    var process = Process.Start(startInfo);
+                    process.WaitForExit();
+                }
+                catch (Win32Exception)
+                {
+                    var failureMessage = isAssociate ? "APK を本アプリに関連付けられませんでした。" : "関連付けの解除に失敗しました。";
+                    Message.Add(failureMessage);
+                    Message.AddEmptyLine();
+                    return;
+                }
+
+                var successMessage = isAssociate ? "関連付けに成功しました。今後は APK を直接ダブルクリックすることでインストールを行えます。" : "関連付けを解除しました。";
+                Message.Add(successMessage);
                 Message.AddEmptyLine();
-                return;
-            }
-
-            var successMessage = isAssociate ? "関連付けに成功しました。今後は APK を直接ダブルクリックすることでインストールを行えます。" : "関連付けを解除しました。";
-            Message.Add(successMessage);
-            Message.AddEmptyLine();
+            });
         }
     }
 }
