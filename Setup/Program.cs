6d7a08707d1356f2f72d9e2a34352c70014f29f4
yutoVR <yuto.fushimi@icloud.com>
2021-12-26 00:17:12 +0900

    アンインストール時に一時フォルダを削除

--- Setup/Program.cs
+++ Setup/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using WixSharp;
+using File = WixSharp.File;
 
 namespace Setup
 {
@@ -10,7 +12,7 @@ namespace Setup
             var exe = new File(@"..\APKInstaller\bin\Release\APKInstaller.exe");
             exe.Shortcuts = new[] { new FileShortcut("APK Installer", "%ProgramMenu%") };
             var files = new Dir(@"%ProgramFiles%\yutokun\APK Installer", exe);
-            var project = new Project("APK Installer", files)
+            var project = new ManagedProject("APK Installer", files)
             {
                 ProductId = new Guid("f9b8f955-f62f-4765-a2e3-c94c198b3eb0"),
                 UpgradeCode = new Guid("f9b8f955-f62f-4765-a2e3-c94c198b3eb1"),
@@ -18,7 +20,17 @@ namespace Setup
                 LicenceFile = "../LICENSE.rtf",
                 Language = "ja-JP"
             };
+            project.AfterInstall += RemoveTempFolder;
             Compiler.BuildMsi(project);
+        }
+
+        static void RemoveTempFolder(SetupEventArgs e)
+        {
+            if (e.IsUninstalling)
+            {
+                var temp = Path.Combine(Path.GetTempPath(), "APKInstaller");
+                Directory.Delete(temp, true);
+            }
         }
     }
 }
