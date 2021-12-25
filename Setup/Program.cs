b119a89d52411d54ba16813291043dc5e9515dcf
yutoVR <yuto.fushimi@icloud.com>
2021-12-25 15:02:12 +0900

    wip: Windows へのインストーラー

---
+++ Setup/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WixSharp;
+
+namespace Setup
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var files = new Dir(@"%ProgramFiles%\yutokun\APK Installer", new File(@"..\APKInstaller\bin\Release\APKInstaller.exe"));
+            var project = new Project("APK Installer", files);
+            project.GUID = new Guid();
+            Compiler.BuildMsi(project);
+        }
+    }
+}
