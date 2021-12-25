c6f646660f81cdb5c2d098c64223a88b7358e2d1
yutoVR <yuto.fushimi@icloud.com>
2021-12-25 23:59:12 +0900

    ProductId と UpgradeCode を個別に指定
    GUID を指定すると ProductId がブレることが分かったので

--- Setup/Program.cs
+++ Setup/Program.cs
@@ -12,7 +12,8 @@ namespace Setup
             var files = new Dir(@"%ProgramFiles%\yutokun\APK Installer", exe);
             var project = new Project("APK Installer", files)
             {
-                GUID = new Guid("f9b8f955-f62f-4765-a2e3-c94c198b3eb0"),
+                ProductId = new Guid("f9b8f955-f62f-4765-a2e3-c94c198b3eb0"),
+                UpgradeCode = new Guid("f9b8f955-f62f-4765-a2e3-c94c198b3eb1"),
                 Version = new Version("1.4.1"),
                 LicenceFile = "../LICENSE.rtf",
                 Language = "ja-JP"
